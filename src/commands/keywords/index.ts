b94f141a68bfcef9778e79d4ad7df123522a6f87
Max Carlquist <tenkaklet@gmail.com>
2024-04-18 11:23:02 +0200

    feat: Add welcome message, inquirer interaction and validation for keywords input in Keywords command. Updates package.json with new dependency 'chalk' and newer version of 'inquirer'.

--- src/commands/keywords/index.ts
+++ src/commands/keywords/index.ts
@@ -1,15 +1,54 @@
 import { Command, Args, Flags } from "@oclif/core";
+import ora from "ora";
+import inquirer from "inquirer";
+import chalk from "chalk";
+
 
 export default class Keywords extends Command {
-    static override args = {
-        check: Args.string({description: 'Input your keywords', required: true}),
-    }
 
-    static readonly description = "Discover different domains from your keywords";
+  // NOTE: domain-seeker keywords check is to instantiate inquirer and the main application.
+  // We Begin with some welcome message and then run the inquirer application.
 
-    async run(): Promise<void> {
-        const {args} = await this.parse(Keywords)
-        // this.log(args) === that the user must put in check to begin inquirer application.
-        this.log(`hello ${args.check}`)
-      }  
+  static readonly description = "Discover different domains from your keywords";
+
+  async run(): Promise<void> {
+    this.spinner('Welcome to Domain Seeker.');
+    this.ask();
+ }
+
+  spinner(message: string) {
+    const spinner = ora(message).start();
+    spinner.color = 'green';
+    spinner.text = message;
+    setTimeout(() => {
+      spinner.succeed(message);
+    }, 500);
+
+    spinner.stop();
+    this.note('Working');
+  }
+
+  note(msg:string) {
+    this.log(`${chalk.red('Welcome to ') } ${chalk.green('Domain') } ${chalk.yellow('Seeker') }`);
+  }
+
+  // Inquier array of keywords.
+  a
+  async ask() {
+    const questions = [
+      {
+        type: 'input',
+        name: 'keywords',
+        message: 'Enter your keywords',
+        validate: (value: string) => {
+          if (value.length < 1) {
+            return 'Please enter keywords';
+          }
+          return true;
+        }
+      }
+    ];
+    const answers = await inquirer.prompt(questions);
+    return answers;
+  }
 }
