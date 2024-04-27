cfda308c34cf4f1cc4cdfce6acc37409c8c7f34c
Max Carlquist <tenkaklet@gmail.com>
2024-04-27 17:26:00 +0200

    Adding

--- src/api.ts
+++ src/api.ts
@@ -4,9 +4,9 @@ import { config } from 'dotenv';
 config();
 
 // Dictionary
-const wordAPIURL:string = 'https://api.datamuse.com/words?ml=';
+const wordAPIURL: string = 'https://api.datamuse.com/words?ml=';
 // if of selected keywords are available
-const searchAPIURL: string = `https://domainr.p.rapidapi.com/v2/search?mashape-key=${process.env.SMASH_KEY}&query=`;
+const searchAPIURL: string = `https://domainr.p.rapidapi.com/v2/register?mashape-key=${process.env.SMASH_KEY}`;
 // if domain is available
 const checkAvailableURL: string = `https://domainr.p.rapidapi.com/v2/status?mashape-key=${process.env.SMASH_KEY}&domain=`;
 /**
@@ -15,18 +15,47 @@ const checkAvailableURL: string = `https://domainr.p.rapidapi.com/v2/status?mash
  * @param {string} word - The word to fetch from the API.
  * @return {Promise<any>} A promise that resolves to the response data.
  */
-export const fetchWord = async ( word: string):Promise<any> => {
+export const fetchWord = async (word: string): Promise<any> => {
     try {
         const response = await fetch('https://api.datamuse.com/words?ml=' + word + '&max=22');
-        if(!response.ok) {
+        if (!response.ok) {
             throw new Error('Error: ' + response.statusText);
         }
         const data = await response.json();
         return data;
     } catch (error) {
-        console.log('error',error);
+        console.log('error', error);
+        throw error;
+    }
+}
+
+export const searchDomains = async (domain: string): Promise<any> => {
+    console.log('domain', domain);
+    
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': process.env.SMASH_KEY ?? '',
+            'x-rapidapi-host': 'domainr.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(`${searchAPIURL + '&domain='}${domain}&registrar=namecheap.com`, options);
+        if (!response.ok) {
+            throw new Error('Error: ' + response.statusText);
+        }
+        console.log('response', response);
+        
+        const data = await response.json();
+        console.log('data', data);
+        
+        return data;
+    } catch (error) {
+        console.log('error', error);
         throw error;
     }
 }
 
 fetchWord(wordAPIURL);
+// searchDomains(searchAPIURL);
--- src/commands/keywords/index.ts
+++ src/commands/keywords/index.ts
@@ -1,15 +1,19 @@
 import { Command, Args, Flags } from "@oclif/core";
-import { fetchWord } from "../../api.js";
+import { fetchWord, searchDomains } from "../../api.js";
 import ora from "ora";
 import inquirer from "inquirer";
 import chalk from "chalk";
 import { DomainWords } from "../../interfaces.js";
 import { iterateArrayWords } from "../../helpers.js";
 import TtyTable from "tty-table";
+import cliProgress from "cli-progress";
 
 export default class Keywords extends Command {
   domainKeywords: string = '';
   suggestedWords: string[] = [];
+  suggestedAvailableWords: string = '';
+  chosenKeywords: string = '';
+  
 
   // NOTE: domain-seeker keywords check is to instantiate inquirer and the main application.
   // We Begin with some welcome message and then run the inquirer application.
@@ -82,20 +86,47 @@ export default class Keywords extends Command {
   chooseWords(words: string[]) {
     inquirer.prompt({ type: 'checkbox', message: 'Choose your keywords that best fit your domain', name: 'chosen-keywords', choices: words }).then((answers) => {
       const chosenKeywords = answers['chosen-keywords'];
+
       // Ora spinner
-      const spinner = ora('Loading domains').start();
+      const spinner = ora('Checking domains').start();
       spinner.color = 'green';
-      setTimeout(() => {
-        spinner.succeed('Suggestions saved!');
-      }, 500);
-      this.createTable(chosenKeywords);
-    })
+      spinner.succeed('Suggestions saved!');
+      this.checkIfWordsAreCorrect(chosenKeywords);
+    });
+  }
+  
+
+  // Creates CLI progress bar and checks if the domain is available.
+
+  // !NOTE: the user can only choose one word to then be able to retrieve the domain endings of .com or .org etc.
+
+
+
+
+  // TODO: check if domain name is avaiable.
+  // TODO: if domain is available, show the cost of the domain.
+  checkIfWordsAreCorrect(domain: string[]) {
+    // loading spinner of org domains
+    inquirer.prompt({ type: 'list', message: 'Are these keywords correct?', name: 'correct-keywords', choices: domain }).then((answers) => {
+      console.log('correct keywords:', answers['correct-keywords']);
+      this.log('Ready to go');
+    });
+    // searchDomains(this.domainKeywords).then((data: string[]) => {
+    //   const array = iterateArrayWords(data);
+    //   this.suggestedWords = array;
+    //   console.log('domain ok', this.suggestedWords);
+    // });
   }
 
   createTable(words: DomainWords[]) {
-    this.log('words', words);
+    // console.log('words', words);
+
+    // check words against API to see if they are available.
+    // TODO: CLI progress bar to check available domains
+
     const header = [
       { value: 'Domain' },
+      { value: 'Cost' },
       { value: 'Available', header: 'available', formatter: (value: string) => chalk.greenBright(value) },
     ];
     // map through each item so a single word can be shown in item
@@ -118,5 +149,8 @@ export default class Keywords extends Command {
     const table = TtyTable(header, rows, [], options).render();
     console.log(table);
   }
+
+
+
 
 }
